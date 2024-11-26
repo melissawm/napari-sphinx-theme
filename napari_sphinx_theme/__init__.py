ea4e27fefdb47813d3fb758c3b45db273fc6464e
Melissa Weber Mendonça <melissawm@gmail.com>
2024-11-26 16:18:01 -0300

    WIP

--- napari_sphinx_theme/__init__.py
+++ napari_sphinx_theme/__init__.py
@@ -2,6 +2,10 @@ import os
 from pathlib import Path
 
 from .napari_code_theme import *
+from sphinx.application import Sphinx
+from typing import Dict, Union
+from bs4 import BeautifulSoup
+
 
 try:
     from ._version import version as __version__
@@ -33,6 +37,27 @@ def update_templates(app, pagename, templatename, context, doctree):
                 if not os.path.splitext(template)[1]:
                     context[section][ii] = template + ".html"
 
+def add_jinja_function(
+        app: Sphinx, pagename: str, templatename: str, context: Dict, doctree
+) -> None:
+    """Add a custom Jinja function to the Sphinx context."""
+
+    def filter_toctree(toctree: str) -> Union[BeautifulSoup, str]:
+        """Filter sidebar-nav-bs toctree to remove full API path for objects
+        """
+        new_toctree = toctree
+        soup = BeautifulSoup(toctree, "html.parser")
+        # for item in soup:
+        #     print(item)
+        #     new_toctree.append(item)
+            # if '<li class="toctree-l3">' in item:
+            #     new_toctree.append(item.split(".")[-1])
+            # else:
+            #     new_toctree.append(item)
+        return toctree
+
+    context["filter_toctree"] = filter_toctree
+
 
 def set_config_defaults(app):
 
@@ -61,6 +86,8 @@ def setup(app):
     app.add_html_theme("napari_sphinx_theme", str(here))
     app.connect("builder-inited", set_config_defaults)
     app.connect("html-page-context", update_templates)
+    app.connect("html-page-context", add_jinja_function)
+
 
     # Include templates for sidebar
     app.config.templates_path.append(str(here / "_templates"))
